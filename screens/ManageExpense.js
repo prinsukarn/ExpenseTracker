f0cb3fc176cf42a588bbf90ce76e0d21b80703ef
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-02-26 14:08:23 +1100

    feat: use route parameters and add different editing modes

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,17 @@
+import { useLayoutEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View style={styles.container}>
       <Text>Manage Expenses Screen</Text>
