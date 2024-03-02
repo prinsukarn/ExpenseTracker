4ad549165391f26be67b3c4f9ef6a4f3c02c11c9
Pratiksha Karn <pratikshakarn@Pratikshas-MacBook-Pro.local>
2024-03-02 18:41:19 +1100

    feat: add input component

---
+++ components/ManageExpense/Input.js
@@ -0,0 +1,14 @@
+import { TextInput, View, StyleSheet } from "react-native";
+
+function Input({ label, textInputConfig }) {
+  return (
+    <View>
+      <Text>{label}</Text>
+      <TextInput {...textInputConfig} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Input;
